89e6c67f0c30822f4d1082f1f4f2ddc12fa91ba8
lcougnaud <laure.cougnaud@openanalytics.eu>
2018-06-25 09:25:52 +0200

    add subject profile event plot (refs #15688)

---
+++ package/patientProfilesVis/R/subjectProfileEventPlot.R
@@ -0,0 +1,63 @@
+#' Create plot of subject profiles for events
+#' @param timeVar string, variable of \code{data} with time
+#' @param shapeVar string, variable of \code{data} for shape of the points
+#' @param shapeLab string, label for \code{shapeVar}
+#' @inheritParams subjectProfileRangePlot
+#' @return list of \code{\link[ggplot2]{ggplot2} objects}
+#' @author Laure Cougnaud
+#' @importFrom glpgUtility getLabelVar
+#' @import ggplot2
+#' @importFrom plyr dlply
+#' @export
+subjectProfileEventPlot <- function(
+	data,
+	paramVar, paramLab = getLabelVar(paramVar, labelVars = labelVars),
+	colorVar = NULL, colorLab = getLabelVar(colorVar, labelVars = labelVars),
+	shapeVar = colorVar, shapeLab = getLabelVar(shapeVar, labelVars = labelVars),
+	timeVar, 
+	subjectVar = "USUBJID",
+	xLab = getLabelVar(timeVar, labelVars = labelVars),
+	yLab = "",
+	title = paramLab,
+	labelVars = NULL
+){
+	
+	if(is.null(colorVar) & is.null(shapeVar))
+		stop("Variable used for coloring and/or for the shape of the points should be specified.")
+	
+	data <- data[with(data, !is.na(get(paramVar)) & !is.na(get(timeVar))), ]
+	
+	if(!is.null(colorVar))	data <- data[!is.na(data[, colorVar]), ]
+	if(!is.null(shapeVar))	data <- data[!is.na(data[, shapeVar]), ]
+	
+	listPlots <- dlply(data, subjectVar, function(dataSubject){	
+				
+		aesArgs <- c(
+			list(x = timeVar, y = paramVar),
+			if(!is.null(colorVar))	list(color = colorVar),
+			if(!is.null(shapeVar))	list(shape = shapeVar)
+		)
+			
+		# create the plot
+		gg <- ggplot(data = dataSubject) +
+			geom_point(
+				do.call(aes_string, aesArgs),
+				size = 3
+			) +
+			scale_y_discrete(drop = TRUE) +
+			theme_bw() +
+			labs(title = title, x = xLab, y = yLab)
+		
+		# change name for color scale
+		if(!is.null(colorLab))
+			gg <- gg + scale_colour_discrete(name = colorLab)
+		
+		# change name for color scale
+		if(!is.null(shapeLab))
+			gg <- gg + scale_shape_discrete(name = shapeLab)
+		
+	})
+	
+	return(listPlots)
+	
+}
