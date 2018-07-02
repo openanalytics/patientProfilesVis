bffee0dc90b23609ebcc090d4c252c1237cb1b48
lcougnaud <laure.cougnaud@openanalytics.eu>
2018-07-02 18:25:28 +0200

    fixes for vignette

--- package/patientProfilesVis/R/data.R
+++ package/patientProfilesVis/R/data.R
@@ -5,12 +5,19 @@
 #' This includes the  adverse event ('ae'), medical history ('mh'), 
 #' demographics ('dm'), and laboratory ('lb') data.
 #' @format list with data.frame for each dataset
-"sdtmDataPelican"
+#' @name sdtmDataPelican
+#' @docType data
+#' @author Laure Cougnaud
+#' @keywords data
+NULL
+
 
 #' Variable labels extracted from the \code{sdtm} datasets for the
 #' Pelican study ('GLPG2737-CL-202-UNBLINDED/CRG10GLP01_SDTM_2018_06JUN_15_v2').
-#'
-#' This includes the  adverse event ('ae'), medical history ('mh'), 
-#' demographics ('dm'), and laboratory ('lb') data.
+#' This corresponds to the \code{\link{sdtmDataPelican}} dataset.
 #' @format named vector with label for each variable (names)
-"labelVarsPelican"
+#' @name labelVarsPelican
+#' @docType data
+#' @author Laure Cougnaud
+#' @keywords data
+NULL
