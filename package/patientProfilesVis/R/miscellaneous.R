4c636dd5c4e41731469df763b1dfc8ee7e5c769e
lcougnaud <laure.cougnaud@openanalytics.eu>
2018-07-02 17:55:37 +0200

    add table of contents in index, decrease margins, and custom dimensions figures in vignette

--- package/patientProfilesVis/R/miscellaneous.R
+++ package/patientProfilesVis/R/miscellaneous.R
@@ -5,3 +5,19 @@
 getPathTemplate <- function(file){
 	system.file(file.path("template", file), package = "patientProfilesVis")
 }
+
+#' Get approximately the number of lines in the y-axis of the plot.
+#' Can be used to specify plot-specific height during the export.
+#' @param gg \code{\link[ggplot2]{ggplot2}} object
+#' @return vector with (approximated) number of lines
+#' @author Laure Cougnaud
+#' @importFrom ggplot2 ggplot_build
+#' @export
+getNLinesYGgplot <- function(gg){
+	nLinesPlot <- length(unique(ggplot_build(gg)$data[[1]]$y))
+	nLinesTitleAndXAxis <- sum(unlist(lapply(ggplot_build(gg)$plot$labels[c("title", "x")], function(label)
+		if(label != "")	length(unlist(strsplit(label, split = "\n"))) * 3
+	)))
+	nLines <- nLinesPlot + nLinesTitleAndXAxis
+	return(nLines)
+}
