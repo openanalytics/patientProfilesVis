a81fbd390c51010a83dc38ca0a7c3ab14ceab9ce
lcougnaud <laure.cougnaud@openanalytics.eu>
2018-10-08 17:56:23 +0200

    include tests visuals (in progress)

---
+++ package/patientProfilesVis/tests/testthat.R
@@ -0,0 +1,7 @@
+library(testthat)
+library(patientProfilesVis)
+test_check("patientProfilesVis")
+
+# visual checking: create first version of the plots
+# validate_cases(cases = collect_new_cases())
+
