b4838520b5875804aebaaf1ea9949528755b5c06
lcougnaud <laure.cougnaud@openanalytics.eu>
2018-10-29 16:22:58 +0100

    add tests for each subject profile plot function + fix for LOCALE to check plot vs reference (fixes #16907) + update script to check package (including coverage fixes #16730)

---
+++ package/patientProfilesVis/tests/testthat/test_subjectProfileIntervalPlot.R
@@ -0,0 +1,97 @@
+context("Compare 'subjectProfileIntervalPlot' with previous version")
+
+# AEPTCD: preferred term code
+dataPlot <- SDTMDataPelican$AE
+# specify order of value in 'AESEV'
+dataPlot[, "AESEV"] <- factor(dataPlot[, "AESEV"], levels = c("MILD", "MODERATE"))
+
+test_that("subjectProfileIntervalPlot - default time limits extraction", {
+			
+	aePlots <- subjectProfileIntervalPlot(
+		data = dataPlot,
+		paramVar = "AETERM",
+		timeStartVar = "AESTDY",
+		timeEndVar = "AEENDY",
+		colorVar = "AESEV",
+		labelVars = labelVarsSDTMPelican,
+		title = "Adverse events"
+	)
+			
+	vdiffr::expect_doppelganger(
+		title = "timeLimitsDefault", 
+		fig = aePlots[[1]][[1]],
+		path = "subjectProfileIntervalPlot",
+		verbose = TRUE
+	)
+			
+})
+
+test_that("subjectProfileIntervalPlot - time limits extracted from a dataset", {
+			
+	aePlotsTimLimFromSV <- subjectProfileIntervalPlot(
+		data = dataPlot,
+		paramVar = "AETERM",
+		timeStartVar = "AESTDY",
+		timeEndVar = "AEENDY",
+		colorVar = "AESEV",
+		labelVars = labelVarsSDTMPelican,
+		title = "Adverse events",
+		timeLimData = SDTMDataPelican$SV,
+		timeLimStartVar = "SVSTDY", 
+		timeLimEndVar = "SVENDY"
+	)
+			
+	vdiffr::expect_doppelganger(
+		title = "timeLimitsFromDataSpecification", 
+		fig = aePlotsTimLimFromSV[[1]][[1]],
+		path = "subjectProfileIntervalPlot",
+		verbose = TRUE
+	)
+			
+})
+
+test_that("subjectProfileIntervalPlot - multiple parameter variables", {
+			
+	# AEPTCD: preferred term codes
+	exPlots <- subjectProfileIntervalPlot(
+		data = SDTMDataPelican$EX,
+		paramVar = c("EXTRT", "EXDOSE", "EXDOSU"),
+		timeStartVar = "EXSTDY",
+		timeEndVar = "EXENDY",
+		colorVar = "EXDOSFRM",
+		labelVars = labelVarsSDTMPelican,
+		title = "Treatment exposure"
+	)
+	
+	vdiffr::expect_doppelganger(
+		title = "paramVarMultiple", 
+		fig = exPlots[[1]][[1]],
+		path = "subjectProfileIntervalPlot",
+		verbose = TRUE
+	)
+			
+})
+
+test_that("subjectProfileIntervalPlot - time limits fixed", {
+			
+	timeLim <- c(-28, 53)
+	cmPlotsTimeSpec <- subjectProfileIntervalPlot(
+		data = SDTMDataPelican$CM,
+		paramVar = c("CMINDC", "CMDECOD", "CMDOSTXT", "CMDOSU", "CMROUTE", "CMDOSFRQ"),
+		timeStartVar = "CMSTDY",
+		timeEndVar = "CMENDY",
+		paramGroupVar = "CMINDC",
+		colorVar = "CMINDC",
+		labelVars = labelVarsSDTMPelican,
+		title = "Concomitant medications",
+		timeLim = timeLim
+	)
+	
+	vdiffr::expect_doppelganger(
+		title = "timeLimitsFixed", 
+		fig = cmPlotsTimeSpec[[1]][[1]],
+		path = "subjectProfileIntervalPlot",
+		verbose = TRUE
+	)
+
+})
---
+++ package/patientProfilesVis/tests/testthat/test_subjectProfileTextPlot.R
@@ -0,0 +1,62 @@
+context("Compare 'subjectProfileTextPlot' with previous version")
+
+test_that("subjectProfileTextPlot - specification of parameter value only", {
+			
+	dmPlots <- subjectProfileTextPlot(
+		data = SDTMDataPelican$DM,
+		paramValueVar = c("SEX|AGE", "RACE|COUNTRY", "ARM"),
+		labelVars = labelVarsSDTMPelican
+	)
+			
+	vdiffr::expect_doppelganger(
+		title = "paramValueVar", 
+		fig = dmPlots[[1]][[1]],
+		path = "subjectProfileTextPlot",
+		verbose = TRUE
+	)
+	
+})
+
+test_that("subjectProfileTextPlot - specification of combination of parameter value and name", {
+			
+	mhPlots <- subjectProfileTextPlot(
+		data = SDTMDataPelican$MH,
+		paramNameVar = "MHTERM",
+		paramValueVar = c("MHCAT", "MHSTDTC", "MHENDTC"),
+		paramGroupVar = "MHCAT",
+		title = "Medical History: status",
+		labelVars = labelVarsSDTMPelican
+	)
+			
+	vdiffr::expect_doppelganger(
+		title = "paramValue/NameVar", 
+		fig = mhPlots[[1]][[1]],
+		path = "subjectProfileTextPlot",
+		verbose = TRUE
+	)
+			
+})
+
+test_that("subjectProfileTextPlot - specification of a function to format parameter", {
+			
+	paramValueVarFct <- function(data)
+		with(data, paste0(MHENRTPT, " (start = ", MHSTDTC, 
+			ifelse(MHENDTC != "", paste0(", end = ", MHENDTC, ")"), ")")
+		)
+	)
+	mhPlotsMultipleVars <- subjectProfileTextPlot(
+		data = SDTMDataPelican$MH,
+		paramNameVar = "MHDECOD",
+		paramValueVar = paramValueVarFct,
+		title = "Medical History: status with dates",
+		labelVars = labelVarsSDTMPelican
+	)
+		
+	vdiffr::expect_doppelganger(
+		title = "paramValueVarAsFunction", 
+		fig = mhPlotsMultipleVars[[1]][[1]],
+		path = "subjectProfileTextPlot",
+		verbose = TRUE
+	)
+			
+})
