153077b8e71a29332e156e70931e799326c34432
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-01-28 15:05:37 +0100

    unit tests: subjectProfileTextPlot: combine paramValueVar, paramValueLab

--- package/patientProfilesVis/tests/testthat/test_subjectProfileTextPlot.R
+++ package/patientProfilesVis/tests/testthat/test_subjectProfileTextPlot.R
@@ -80,3 +80,56 @@ test_that("parameter values are displayed", {
 	}
 		
 })
+
+test_that("parameter values are combined", {
+			
+	data <- data.frame(
+		SEX = "M", AGE = NA_character_,
+		WEIGHT = 40, USUBJID = "1"
+	)
+	plots <- subjectProfileTextPlot(
+		data = data,
+		paramValueVar = "WEIGHT|AGE|SEX"
+	)
+	gg <- plots[["1"]][[1]]
+	
+	# extract data behind the text
+	isGeomText <- sapply(gg$layers, function(l) inherits(l$geom, "GeomText"))
+	ggDataText <- layer_data(gg, which(isGeomText))
+	ggDataText <- ggDataText[order(ggDataText$y), ]
+	
+	yValue <- as.character(ggDataText[, "label"])
+	expect_equal(yValue, c("40 - NA - M"))
+	
+	yLabel <- layer_scales(gg, which(isGeomText))$y$range$range
+	expect_equal(yLabel, c("WEIGHT|AGE|SEX"))
+		
+})
+
+test_that("label for parameters are specified", {
+			
+	data <- data.frame(
+		SEX = "M", AGE = NA_character_,
+		WEIGHT = 40, USUBJID = "1"
+	)
+	paramValueLab <- c(AGE = "Age (years)", WEIGHT = "Weight (kg)")
+	plots <- subjectProfileTextPlot(
+		data = data,
+		paramValueVar = "WEIGHT|AGE|SEX",
+		paramValueLab = paramValueLab
+	)
+	
+	gg <- plots[["1"]][[1]]
+			
+	# extract data behind the text
+	isGeomText <- sapply(gg$layers, function(l) inherits(l$geom, "GeomText"))
+	ggDataText <- layer_data(gg, which(isGeomText))
+	ggDataText <- ggDataText[order(ggDataText$y), ]
+			
+	yValue <- as.character(ggDataText[, "label"])
+	expect_equal(yValue, c("40 - NA - M"))
+			
+	yLabel <- layer_scales(gg, which(isGeomText))$y$range$range
+	expect_equal(yLabel, c("Weight (kg), Age (years), SEX"))
+			
+})
