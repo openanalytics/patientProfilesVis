533edbc8b0ad2bca19d790ef94902a38cfb9897f
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-01-29 11:27:23 +0100

    remove visual tests with vdiffr (replacement in progress)

--- package/patientProfilesVis/tests/testthat.R
+++ package/patientProfilesVis/tests/testthat.R
@@ -1,6 +1,5 @@
 library(testthat)
 library(patientProfilesVis)
-library(vdiffr)
 library(glpgUtilityFct)
 
 # The same collate statement should be used than the R CMD check
@@ -14,20 +13,9 @@ data(SDTMDataPelican)
 # and corresponding labels
 data(labelVarsSDTMPelican)
 
-# to have failed tests with vdiffr not skipped
-Sys.setenv("CI" = "true")
-
 if (Sys.getenv("TESTTHAT_OUTPUT_FILE") != "")
 	options(testthat.output_file = Sys.getenv("TESTTHAT_OUTPUT_FILE", stdout()))
 test_check(
 		"patientProfilesVis",
 		reporter = Sys.getenv("TESTTHAT_DEFAULT_CHECK_REPORTER", "check"))
-
-
-# create figures for new test:
-# manage_cases()
-# run tests for the package:
-# devtools::test()
-# run all R CMD CHECK
-# devtools::check(pkg = ".")
 
--- package/patientProfilesVis/tests/testthat/test_subjectProfileLinePlot.R
+++ package/patientProfilesVis/tests/testthat/test_subjectProfileLinePlot.R
@@ -1,139 +1,139 @@
-context("Compare 'subjectProfileLinePlot' with previous version")
-
-library(glpgUtilityFct)
-library(ggplot2)
-library(plyr)
-data(SDTMDataPelican)
-data(labelVarsSDTMPelican)
-
-# prepare data for plots:
-dataLB <- SDTMDataPelican$LB
-# sort the categories (empty values '' becomes NA)
-dataLB$LBNRIND <- factor(dataLB$LBNRIND, levels = c("LOW", "NORMAL", "HIGH"))
-
-dataLB$LBTEST <- with(dataLB, reorder(LBTEST, LBTESTCD, unique))		
-
-dataPlot <- subset(dataLB, USUBJID == "study-4902-02")
-dataPlot <- subset(dataPlot, LBTEST %in% unique(dataPlot[, "LBTEST"])[1:5])
-rownames(dataPlot) <- NULL
-
-test_that("subjectProfileLinePlot - basic plot", {
-					
-	title <- "Laboratory test measurements: actual value"
-	expect_error(
-		lbLinePlots <- subjectProfileLinePlot(
-			data = dataPlot,
-			paramNameVar = "LBTEST", 
-			paramValueVar = "LBSTRESN",
-			paramValueRangeVar = c("LBSTNRLO", "LBSTNRHI"),
-			timeVar = "LBDY",
-			title = title,
-			labelVars = labelVarsSDTMPelican
-		),
-		NA
-	)
-	
-	gg <- lbLinePlots[[1]][[1]]
-	ggData <- gg$data[, colnames(dataPlot)]
-	expect_equal(ggData, dataPlot)
-
-	expect_identical(gg$labels$title, title)
-	
-})
-
-test_that("subjectProfileLinePlot - color/shape variable and palette specification", {
-			
-	expect_error(
-		lbLinePlotsColorShape <- subjectProfileLinePlot(
-			data = dataPlot,
-			paramNameVar = "LBTEST", 
-			paramValueVar = "LBSTRESN",
-			colorVar = "LBNRIND",
-			shapeVar = "LBNRIND",
-			shapePalette = c('LOW' = 25, 'NORMAL' = 19, 'HIGH' = 24),
-			paramGroupVar = "LBSCAT",
-			paramValueRangeVar = c("LBSTNRLO", "LBSTNRHI"),
-			timeVar = "LBDY",
-			title = "Laboratory test measurements: actual value",
-			labelVars = labelVarsSDTMPelican
-		),
-		NA
-	)
-	
-	gg <- lbLinePlotsColorShape[[1]][[1]]
-	ggData <- gg$data[, colnames(dataPlot)]
-	expect_equal(colwise(as.character)(ggData), colwise(as.character)(dataPlot))
-	
-})
-
-test_that("subjectProfileLinePlot - yLimFrom - value", {
-			
-	expect_error(
-		lbLinePlots <- subjectProfileLinePlot(
-			data = dataPlot,
-			paramNameVar = "LBTEST", 
-			paramValueVar = "LBSTRESN",
-			paramValueRangeVar = c("LBSTNRLO", "LBSTNRHI"),
-			timeVar = "LBDY",
-			title = "Laboratory test measurements: actual value",
-			labelVars = labelVarsSDTMPelican,
-			yLimFrom = "value"
-		),
-		NA
-	)
-	
-	# output data from ggplot2
+#context("Compare 'subjectProfileLinePlot' with previous version")
+#
+#library(glpgUtilityFct)
+#library(ggplot2)
+#library(plyr)
+#data(SDTMDataPelican)
+#data(labelVarsSDTMPelican)
+#
+## prepare data for plots:
+#dataLB <- SDTMDataPelican$LB
+## sort the categories (empty values '' becomes NA)
+#dataLB$LBNRIND <- factor(dataLB$LBNRIND, levels = c("LOW", "NORMAL", "HIGH"))
+#
+#dataLB$LBTEST <- with(dataLB, reorder(LBTEST, LBTESTCD, unique))		
+#
+#dataPlot <- subset(dataLB, USUBJID == "study-4902-02")
+#dataPlot <- subset(dataPlot, LBTEST %in% unique(dataPlot[, "LBTEST"])[1:5])
+#rownames(dataPlot) <- NULL
+#
+#test_that("subjectProfileLinePlot - basic plot", {
+#					
+#	title <- "Laboratory test measurements: actual value"
+#	expect_error(
+#		lbLinePlots <- subjectProfileLinePlot(
+#			data = dataPlot,
+#			paramNameVar = "LBTEST", 
+#			paramValueVar = "LBSTRESN",
+#			paramValueRangeVar = c("LBSTNRLO", "LBSTNRHI"),
+#			timeVar = "LBDY",
+#			title = title,
+#			labelVars = labelVarsSDTMPelican
+#		),
+#		NA
+#	)
+#	
 #	gg <- lbLinePlots[[1]][[1]]
-#	ggBuildData <- ggplot_build(gg)$data
-#	dataPlot <- ggBuildData[[which.max(sapply(ggBuildData, nrow))]]
-#	dataPlot <- dataPlot[, c("PANEL", "x", "y")]
-			
-})
-
-test_that("subjectProfileLinePlot - correct order of paramGroupVar", {
-		
-	# create the plot:
-	dataPatient <- subset(dataLB, USUBJID == "study-4902-02")
-	paramGroupVar <- c("LBCAT", "LBSCAT")
-	xVar <- "LBDY"
-	yVar <- "LBSTRESN"
-	paramVar <- "LBTEST"
-	lbLinePlots <- subjectProfileLinePlot(
-		data = dataPatient,
-		paramNameVar = paramVar, 
-		paramValueVar = yVar,
-		paramGroupVar = paramGroupVar,
-		timeVar = xVar,
-		title = "Laboratory test measurements: actual value",
-		labelVars = labelVarsSDTMPelican
-	)
-	
-	# output data from ggplot2
-	dataPlotsList <- lapply(seq_along(lbLinePlots[[1]]), function(i){
-		gg <- lbLinePlots[[1]][[i]]
-		ggBuildData <- ggplot_build(gg)$data
-		dataPlot <- ggBuildData[[which.max(sapply(ggBuildData, nrow))]]
-		dataPlot <- dataPlot[, c("PANEL", "x", "y")]
-		dataPlot$PANEL <- as.numeric(dataPlot$PANEL) + i * 1000
-		dataPlot
-	})
-	dataPlots <- do.call(rbind, dataPlotsList)
-	dataPlotsOrd <- dataPlots[do.call(order, dataPlots), ]
-	rownames(dataPlotsOrd) <- NULL
-	
-	# input data for plot:
-	dataInputPlot <- dataPatient[, c(paramGroupVar, paramVar, xVar, yVar)]
-	idxComplete <- which(!is.na(dataInputPlot[, xVar]) & !is.na(dataInputPlot[, yVar]))
-	dataInputPlot <- dataInputPlot[idxComplete, ]
-	idxOrderParam <- do.call(order, dataInputPlot[, c(paramGroupVar, paramVar)])	
-	paramOrder <- unique(dataInputPlot[idxOrderParam, paramVar])
-	dataInputPlot$PANEL <- match(dataInputPlot[, paramVar], paramOrder)
-	dataInputPlotOrd <- dataInputPlot[do.call(order, dataInputPlot[, c("PANEL", xVar, yVar)]), ]
-	
-	# compare the two
-	expect_equivalent(
-		object = dataPlotsOrd[, c("x", "y")],
-		expected = dataInputPlotOrd[, c(xVar, yVar)]
-	)
-	
-})
+#	ggData <- gg$data[, colnames(dataPlot)]
+#	expect_equal(ggData, dataPlot)
+#
+#	expect_identical(gg$labels$title, title)
+#	
+#})
+#
+#test_that("subjectProfileLinePlot - color/shape variable and palette specification", {
+#			
+#	expect_error(
+#		lbLinePlotsColorShape <- subjectProfileLinePlot(
+#			data = dataPlot,
+#			paramNameVar = "LBTEST", 
+#			paramValueVar = "LBSTRESN",
+#			colorVar = "LBNRIND",
+#			shapeVar = "LBNRIND",
+#			shapePalette = c('LOW' = 25, 'NORMAL' = 19, 'HIGH' = 24),
+#			paramGroupVar = "LBSCAT",
+#			paramValueRangeVar = c("LBSTNRLO", "LBSTNRHI"),
+#			timeVar = "LBDY",
+#			title = "Laboratory test measurements: actual value",
+#			labelVars = labelVarsSDTMPelican
+#		),
+#		NA
+#	)
+#	
+#	gg <- lbLinePlotsColorShape[[1]][[1]]
+#	ggData <- gg$data[, colnames(dataPlot)]
+#	expect_equal(colwise(as.character)(ggData), colwise(as.character)(dataPlot))
+#	
+#})
+#
+#test_that("subjectProfileLinePlot - yLimFrom - value", {
+#			
+#	expect_error(
+#		lbLinePlots <- subjectProfileLinePlot(
+#			data = dataPlot,
+#			paramNameVar = "LBTEST", 
+#			paramValueVar = "LBSTRESN",
+#			paramValueRangeVar = c("LBSTNRLO", "LBSTNRHI"),
+#			timeVar = "LBDY",
+#			title = "Laboratory test measurements: actual value",
+#			labelVars = labelVarsSDTMPelican,
+#			yLimFrom = "value"
+#		),
+#		NA
+#	)
+#	
+#	# output data from ggplot2
+##	gg <- lbLinePlots[[1]][[1]]
+##	ggBuildData <- ggplot_build(gg)$data
+##	dataPlot <- ggBuildData[[which.max(sapply(ggBuildData, nrow))]]
+##	dataPlot <- dataPlot[, c("PANEL", "x", "y")]
+#			
+#})
+#
+#test_that("subjectProfileLinePlot - correct order of paramGroupVar", {
+#		
+#	# create the plot:
+#	dataPatient <- subset(dataLB, USUBJID == "study-4902-02")
+#	paramGroupVar <- c("LBCAT", "LBSCAT")
+#	xVar <- "LBDY"
+#	yVar <- "LBSTRESN"
+#	paramVar <- "LBTEST"
+#	lbLinePlots <- subjectProfileLinePlot(
+#		data = dataPatient,
+#		paramNameVar = paramVar, 
+#		paramValueVar = yVar,
+#		paramGroupVar = paramGroupVar,
+#		timeVar = xVar,
+#		title = "Laboratory test measurements: actual value",
+#		labelVars = labelVarsSDTMPelican
+#	)
+#	
+#	# output data from ggplot2
+#	dataPlotsList <- lapply(seq_along(lbLinePlots[[1]]), function(i){
+#		gg <- lbLinePlots[[1]][[i]]
+#		ggBuildData <- ggplot_build(gg)$data
+#		dataPlot <- ggBuildData[[which.max(sapply(ggBuildData, nrow))]]
+#		dataPlot <- dataPlot[, c("PANEL", "x", "y")]
+#		dataPlot$PANEL <- as.numeric(dataPlot$PANEL) + i * 1000
+#		dataPlot
+#	})
+#	dataPlots <- do.call(rbind, dataPlotsList)
+#	dataPlotsOrd <- dataPlots[do.call(order, dataPlots), ]
+#	rownames(dataPlotsOrd) <- NULL
+#	
+#	# input data for plot:
+#	dataInputPlot <- dataPatient[, c(paramGroupVar, paramVar, xVar, yVar)]
+#	idxComplete <- which(!is.na(dataInputPlot[, xVar]) & !is.na(dataInputPlot[, yVar]))
+#	dataInputPlot <- dataInputPlot[idxComplete, ]
+#	idxOrderParam <- do.call(order, dataInputPlot[, c(paramGroupVar, paramVar)])	
+#	paramOrder <- unique(dataInputPlot[idxOrderParam, paramVar])
+#	dataInputPlot$PANEL <- match(dataInputPlot[, paramVar], paramOrder)
+#	dataInputPlotOrd <- dataInputPlot[do.call(order, dataInputPlot[, c("PANEL", xVar, yVar)]), ]
+#	
+#	# compare the two
+#	expect_equivalent(
+#		object = dataPlotsOrd[, c("x", "y")],
+#		expected = dataInputPlotOrd[, c(xVar, yVar)]
+#	)
+#	
+#})
