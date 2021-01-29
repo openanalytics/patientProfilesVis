6644e6cf0578f6fcc9644278bf5e9d4d74319680
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-01-29 11:27:23 +0100

    remove visual tests with vdiffr (replacement in progress)

--- package/patientProfilesVis/tests/testthat/test_subjectProfileEventPlot.R
+++ package/patientProfilesVis/tests/testthat/test_subjectProfileEventPlot.R
@@ -1,87 +1,87 @@
-context("Compare 'subjectProfileEventPlot' with previous version")
-
-library(glpgUtilityFct)
-data(SDTMDataPelican)
-data(labelVarsSDTMPelican)
-
-# prepare data for plots:
-dataLB <- SDTMDataPelican$LB
-# sort the categories (empty values '' becomes NA)
-dataLB$LBNRIND <- factor(dataLB$LBNRIND, levels = c("LOW", "NORMAL", "HIGH"))
-
-test_that("subjectProfileEventPlot - basic plot", {
-					
-	# create plot
-	lbPlots <- subjectProfileEventPlot(
-		data = dataLB,
-		paramVar = c("LBSCAT", "LBTEST"),
-		paramGroupVar = "LBSCAT",
-		timeVar = "LBDY",
-		labelVars = labelVarsSDTMPelican,
-		title = "Laboratory test measurements"
-	)
-			
-	vdiffr::expect_doppelganger(
-		title = "basic", 
-		fig = lbPlots[[1]][[1]],
-		path = "subjectProfileEventPlot",
-		verbose = TRUE
-	)
-	
-})
-
-test_that("subjectProfileEventPlot - coloring variable", {
-			
-	dataPlot <- dataLB
-			
-	# fix for ordering cat in tests
-	dataPlot$LBTEST <- with(dataPlot, reorder(LBTEST, LBTESTCD, unique))		
-			
-	lbPlotsColor <- subjectProfileEventPlot(
-		data = dataPlot,
-		paramVar = "LBTEST",
-		paramGroupVar = "LBSCAT",
-		timeVar = "LBDY",
-		colorVar = "LBNRIND",
-		labelVars = labelVarsSDTMPelican,
-		shapeVar = "LBNRIND",
-		shapePalette = c('LOW' = 25, 'NORMAL' = 19, 'HIGH' = 24, 'NA' = 3),
-		title = "Laboratory test measurements: reference range indicator"
-	)
-	
-	vdiffr::expect_doppelganger(
-		title = "colorVariable", 
-		fig = lbPlotsColor[[1]][[1]],
-		path = "subjectProfileEventPlot",
-		verbose = TRUE
-	)
-	
-})
-
-test_that("subjectProfileEventPlot - subset variable", {
-	
-	dataPlot <- dataLB
-	
-	# fix for ordering cat in tests
-	dataPlot$LBTEST <- with(dataPlot, reorder(LBTEST, LBTESTCD, unique))
-	
-	lbPlotsSubset <- subjectProfileEventPlot(
-		data = dataPlot,
-		paramVar = "LBTEST",
-		subsetVar = "LBCAT", subsetValue = "HEMATOLOGY",
-		timeVar = "LBDY",
-		colorVar = "LBNRIND",
-		labelVars = labelVarsSDTMPelican,
-		shapeVar = "LBNRIND",
-		shapePalette = c('LOW' = 25, 'NORMAL' = 19, 'HIGH' = 24, 'NA' = 3),
-		title = "Hematology test measurements: reference range indicator"
-	)
-	
-	vdiffr::expect_doppelganger(
-		title = "subsetVariable", 
-		fig = lbPlotsSubset[[1]][[1]],
-		path = "subjectProfileEventPlot",
-		verbose = TRUE
-	)
-	
-})
+#context("Compare 'subjectProfileEventPlot' with previous version")
+#
+#library(glpgUtilityFct)
+#data(SDTMDataPelican)
+#data(labelVarsSDTMPelican)
+#
+## prepare data for plots:
+#dataLB <- SDTMDataPelican$LB
+## sort the categories (empty values '' becomes NA)
+#dataLB$LBNRIND <- factor(dataLB$LBNRIND, levels = c("LOW", "NORMAL", "HIGH"))
+#
+#test_that("subjectProfileEventPlot - basic plot", {
+#					
+#	# create plot
+#	lbPlots <- subjectProfileEventPlot(
+#		data = dataLB,
+#		paramVar = c("LBSCAT", "LBTEST"),
+#		paramGroupVar = "LBSCAT",
+#		timeVar = "LBDY",
+#		labelVars = labelVarsSDTMPelican,
+#		title = "Laboratory test measurements"
+#	)
+#			
+#	vdiffr::expect_doppelganger(
+#		title = "basic", 
+#		fig = lbPlots[[1]][[1]],
+#		path = "subjectProfileEventPlot",
+#		verbose = TRUE
+#	)
+#	
+#})
+#
+#test_that("subjectProfileEventPlot - coloring variable", {
+#			
+#	dataPlot <- dataLB
+#			
+#	# fix for ordering cat in tests
+#	dataPlot$LBTEST <- with(dataPlot, reorder(LBTEST, LBTESTCD, unique))		
+#			
+#	lbPlotsColor <- subjectProfileEventPlot(
+#		data = dataPlot,
+#		paramVar = "LBTEST",
+#		paramGroupVar = "LBSCAT",
+#		timeVar = "LBDY",
+#		colorVar = "LBNRIND",
+#		labelVars = labelVarsSDTMPelican,
+#		shapeVar = "LBNRIND",
+#		shapePalette = c('LOW' = 25, 'NORMAL' = 19, 'HIGH' = 24, 'NA' = 3),
+#		title = "Laboratory test measurements: reference range indicator"
+#	)
+#	
+#	vdiffr::expect_doppelganger(
+#		title = "colorVariable", 
+#		fig = lbPlotsColor[[1]][[1]],
+#		path = "subjectProfileEventPlot",
+#		verbose = TRUE
+#	)
+#	
+#})
+#
+#test_that("subjectProfileEventPlot - subset variable", {
+#	
+#	dataPlot <- dataLB
+#	
+#	# fix for ordering cat in tests
+#	dataPlot$LBTEST <- with(dataPlot, reorder(LBTEST, LBTESTCD, unique))
+#	
+#	lbPlotsSubset <- subjectProfileEventPlot(
+#		data = dataPlot,
+#		paramVar = "LBTEST",
+#		subsetVar = "LBCAT", subsetValue = "HEMATOLOGY",
+#		timeVar = "LBDY",
+#		colorVar = "LBNRIND",
+#		labelVars = labelVarsSDTMPelican,
+#		shapeVar = "LBNRIND",
+#		shapePalette = c('LOW' = 25, 'NORMAL' = 19, 'HIGH' = 24, 'NA' = 3),
+#		title = "Hematology test measurements: reference range indicator"
+#	)
+#	
+#	vdiffr::expect_doppelganger(
+#		title = "subsetVariable", 
+#		fig = lbPlotsSubset[[1]][[1]],
+#		path = "subjectProfileEventPlot",
+#		verbose = TRUE
+#	)
+#	
+#})
