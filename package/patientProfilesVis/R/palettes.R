82e15474841fb8d4e18d122cb2b95933a18ce22b
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-03-02 15:53:53 +0100

    unit tests: linetype palette

--- package/patientProfilesVis/R/palettes.R
+++ package/patientProfilesVis/R/palettes.R
@@ -1,8 +1,9 @@
 #' Get a Galapagos color palette of specified length
 #' for patient profile visualization visualization.
+#' 
 #' This is only a wrapper above \code{\link[glpgStyle]{getGLPGColorPalette}},
 #' including by default missing values (contrary to the default of the
-#' code{\link[glpgStyle]{getGLPGColorPalette}} function).
+#' \code{\link[glpgStyle]{getGLPGColorPalette}} function).
 #' @param includeNA Logical (TRUE by default), should NA elements be retained in the palette.
 #' @param ... Additional parameters for the 
 #' \code{\link[glpgStyle]{getGLPGColorPalette}} function.
@@ -21,9 +22,10 @@ getGLPGColorPalettePatientProfile <- function(...,
 
 #' Get a Galapagos linetype palette palette of specified length
 #' for patient profile visualization visualization.
+#' 
 #' This is only a wrapper above \code{\link[glpgStyle]{getGLPGLinetypePalette}},
 #' including by default missing values (contrary to the default of the
-#' code{\link[glpgStyle]{getGLPGLinetypePalette}} function).
+#' \code{\link[glpgStyle]{getGLPGLinetypePalette}} function).
 #' @param ... Additional parameters for the 
 #' \code{\link[glpgStyle]{getGLPGLinetypePalette}} function.
 #' @inheritParams getGLPGColorPalettePatientProfile
@@ -42,13 +44,11 @@ getGLPGLinetypePalettePatientProfile <- function(...,
 
 #' Get a Galapagos shape palette palette of specified length
 #' for patient profile visualization visualization.
-#' This is only a wrapper above \code{\link[glpgStyle]{getGLPGShapePalette}},
-#' including by default missing values (contrary to the default of the
-#' code{\link[glpgStyle]{getGLPGShapePalette}} function).
 #' 
 #' This is only a wrapper above \code{\link[glpgStyle]{getGLPGShapePalette}},
-#' including by default missing values and formatting the symbols as text
-#' (in case mixed with Unicode).
+#' including by default missing values (contrary to the default of the
+#' code{\link[glpgStyle]{getGLPGShapePalette}} function) 
+#' and formatting the symbols as text (in case mixed with Unicode).
 #' @param asText Logical (TRUE by default), should the palette
 #' be expressed as integer (base R plot and ggplot2 compatible)
 #' or in text format (e.g. required if combined with unicode symbols in ggplot2)?
