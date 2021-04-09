0142b988fcf6989f089d891ba2db44556a2878a4
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-04-09 10:34:49 +0200

    update unit tests

--- package/patientProfilesVis/tests/testthat/test_palettes.R
+++ package/patientProfilesVis/tests/testthat/test_palettes.R
@@ -4,10 +4,10 @@ library(viridisLite)
 
 test_that("Missing values in palette is extracted as specified", {
       xWithNA <- c(NA_character_, "group1")
-      expect_false('NA' %in% names(getColorPalettePatientProfile(x = xWithNA)))
-      expect_true('NA' %in% names(getColorPalettePatientProfile(x = xWithNA, includeNA = TRUE)))
-      expect_false('NA' %in% names(getShapePalettePatientProfile(x = xWithNA)))
-      expect_true('NA' %in% names(getShapePalettePatientProfile(x = xWithNA, includeNA = TRUE)))
+      expect_false('NA' %in% names(getColorPalettePatientProfile(x = xWithNA, includeNA = FALSE)))
+      expect_true('NA' %in% names(getColorPalettePatientProfile(x = xWithNA)))
+      expect_false('NA' %in% names(getShapePalettePatientProfile(x = xWithNA, includeNA = FALSE)))
+      expect_true('NA' %in% names(getShapePalettePatientProfile(x = xWithNA)))
     })
 
 test_that("Empty values in palette is retained", {
@@ -17,19 +17,16 @@ test_that("Empty values in palette is retained", {
       xWithEmpty <- c("", "group1")
       expect_silent(palette <- getShapePalettePatientProfile(x = xWithEmpty))
       expect_equal(names(palette), xWithEmpty)
-      xWithEmpty <- c("", "group1")
-      expect_silent(palette <- getGLPGLinetypePalette(x = xWithEmpty))
-      expect_equal(names(palette), xWithEmpty)
     })
 
 test_that("Default palette is viridis", {
       
       expect_identical(
-          getColorPalettePatientProfile(n = 1, type = "viridis"),
+          getColorPalettePatientProfile(n = 1),
           viridis(n = 1)
       )     
       expect_identical(
-          getColorPalettePatientProfile(n = 2, type = "viridis"),
+          getColorPalettePatientProfile(n = 2),
           viridis(n = 2)
       )
       
