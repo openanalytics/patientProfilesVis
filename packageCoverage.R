77b00501d01a8b1c623620307c7a365d53f6c3eb
Laure Cougnaud <laure.cougnaud@openanalytics.eu>
2022-02-01 11:26:44 +0100

    remove packageCoverage (available in testPackages)

--- packageCoverage.R
+++
@@ -1,9 +0,0 @@
-library(covr)
-pc <- package_coverage(
-    path = "~/git/patientProfilesVis/package/patientProfilesVis",
-    type = "tests", quiet = FALSE, clean = FALSE
-)
-report(x = pc,
-    file = paste0("testCoverage-tests-patientProfilesVis",
-        packageVersion("patientProfilesVis"), ".html")
-)
